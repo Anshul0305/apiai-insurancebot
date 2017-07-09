36a7eb95981d5c55b0ac6e97dc3d7b21d263229e
Anshul Shrivastava <anshul.shrivastava123@gmail.com>
2017-07-09 05:21:19 +0100

    insurance chatbot

--- index.php
+++ index.php
@@ -3,18 +3,50 @@
 
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
+  // Get the json for APIAI
   $requestBody = file_get_contents('php://input');
   $json = json_decode($requestBody);
-  $busRoute = $json->result->parameters->BusRoute;
-  $destination = $json->result->parameters->BusDestination;
-  $speech = "Next ".$busRoute." Towards ".$destination." will come late....";
-  
+
+  // Fetch the params from the json
+  $insuranceType = $json->result->parameters->insuranceType;
+  $healthInsuranceFor = $json->result->parameters->healthInsuranceFor;
+  $travelInsuranceDestination = $json->result->parameters->travelInsuranceDestination;
+
+  // Process the response and add business logic
+  switch ($insuranceType) {
+    case 'travel':
+      $speech = "So you need travel insurance";
+      break;
+
+    case 'health':
+      $speech = "So you need health insurance";
+      break;
+    
+    case 'pet':
+      $speech = "So you need pet insurance";
+      break;
+    
+    case 'property':
+      $speech = "So you need property insurance";
+      break;
+
+    case 'motor':
+      $speech = "So you need motor insurance";
+      break;
+    
+    default:
+      $speech = "Sorry, couldnt get you. Please let me know what insurance do you need?";
+      break;
+  }
+
+  // Create the speech response
   $response = new \stdClass();
   $response->speech = $speech;
   $response->displayText = $speech;
-  $response->source = "apiai-php-bustimer";
+  $response->source = "apiai-php-insurance-chatbot";
+
+  // Create and Send the json back to APIAI
   $jsonResponse = json_encode($response);
- 
   echo $jsonResponse;
 }
 else
